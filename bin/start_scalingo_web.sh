40cbb981317ba3fac3fa3a5f668a4b5763c26b9f
Eric Farges <eric.farges@gmail.com>
2023-06-19 10:13:15 +0200

    Update scalingo deployment scripts

--- bin/start_scalingo_web.sh
+++ bin/start_scalingo_web.sh
@@ -7,8 +7,10 @@ echo "Démarrage de l'instance $APP_HOST"
 if [ $IS_REVIEW_APP ]; then
     export DISABLE_DATABASE_ENVIRONMENT_CHECK=1
     bin/rails db:schema:load
-    bin/rails db:migrate
+    bin/rails db:migrations:setup
+    bin/migrate-data.sh
     bin/rails db:seed
 fi
 
-bundle exec puma -C config/puma.rb
+# bundle exec puma -C config/puma.rb
+bundle exec rails server
