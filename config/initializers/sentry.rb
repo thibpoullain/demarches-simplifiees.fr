f89906320517ed3ec64c44fb31e5ca9322085d9f
Eric Farges <eric.farges@gmail.com>
2023-06-21 11:12:47 +0200

    Update Gemfile.lock and sentry.rb config from latest Dinum adapted to demat

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -22,27 +22,34 @@ Sentry.init do |config|
   config.enabled_environments = ['production', secrets[:environment].presence].compact
   config.breadcrumbs_logger = [:active_support_logger]
 
-  # transaction_context is the transaction object in hash form
-  # keep in mind that sampling happens right after the transaction is initialized
-  # for example, at the beginning of the request
-  transaction_context = sampling_context[:transaction_context]
+  config.traces_sampler = lambda do |sampling_context|
+    # if this is the continuation of a trace, just use that decision (rate controlled by the caller)
+    unless sampling_context[:parent_sampled].nil?
+      next sampling_context[:parent_sampled]
+    end
 
-  # transaction_context helps you sample transactions with more sophistication
-  # for example, you can provide different sample rates based on the operation or name
-  case transaction_context[:op]
-  when /delayed_job/
-    contexts = Sentry.get_current_scope.contexts
-    job_class = contexts.dig(:"Active-Job", :job_class)
-    attempts = contexts.dig(:"Delayed-Job", :attempts)
-    max_attempts = job_class.safe_constantize&.new&.max_attempts rescue 25
+    # transaction_context is the transaction object in hash form
+    # keep in mind that sampling happens right after the transaction is initialized
+    # for example, at the beginning of the request
+    transaction_context = sampling_context[:transaction_context]
 
-    # Don't trace on all attempts
-    [0, 2, 5, 10, 20, max_attempts].include?(attempts)
-  else # rails requests
-    if sampling_context.dig(:env, "REQUEST_METHOD") == "GET"
-      0.001
-    else
-      0.01
+    # transaction_context helps you sample transactions with more sophistication
+    # for example, you can provide different sample rates based on the operation or name
+    case transaction_context[:op]
+    when /delayed_job/
+      contexts = Sentry.get_current_scope.contexts
+      job_class = contexts.dig(:"Active-Job", :job_class)
+      attempts = contexts.dig(:"Delayed-Job", :attempts)
+      max_attempts = job_class.safe_constantize&.new&.max_attempts rescue 25
+
+      # Don't trace on all attempts
+      [0, 2, 5, 10, 20, max_attempts].include?(attempts)
+    else # rails requests
+      if sampling_context.dig(:env, "REQUEST_METHOD") == "GET"
+        0.001
+      else
+        0.01
+      end
     end
   end
 
