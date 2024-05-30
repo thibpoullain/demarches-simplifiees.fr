1a7b9f5edf6a0db7adcc91d2a4c286366b40699d
Thibaut Poullain <thibaut.poullain@ydrazil.fr>
2024-05-30 08:37:18 +0200

    🛠️ Data migration | Update the data migration mecanism

--- bin/migrate-data.sh
+++ bin/migrate-data.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
-FILE_LIST="./bin/migrations-and-tasks-list.txt"
+# Lister les migrations de db (db:migrate) et de données (after_party) qui n'ont pas encore été effectuée sur l'instance
+# Tri par date croissante
+# Obtention d'une liste type :
+# 20220405163206,00_db_migrate 20220407081538,00_db_migrate 20220408100411,01_after_party ...
+migrations=$(( bin/rake db:migrate:status | grep down | grep -oP '([0-9]){14}' | sed 's/$/,00_db_migrate/' ; bin/rake after_party:status | grep down | grep -oP '([0-9]){14}' | sed 's/$/,01_after_party/') | cat | sort)
 
-# For testing purposes
-#FILE_LIST="./bin/migrations-sample-list.txt"
+echo "Migrations (schéma et données) à appliquer : "
+echo $migrations
 
-echo "Processing data migrations and after party tasks..."
-while IFS= read -r file
-do
-  if [[ $file == *.rb ]]
+# Itération pour application des migrations, dans l'ordre donné
+# Une erreur de migration de schéma est BLOQUANTE
+# Une erreur de migration de données ne l'est pas (la migration se poursuit ; une analyse humaine est alors à prévoir)
+# En cas d'erreur des commandes rails, les logs apparaissent dans la sortie standard + reversées sur Sentry (si configuré)
+for migration in $migrations ; do
+  # Itération sur les espaces (couple id / type) puis "split" sur la virgule
+  id_migration=${migration%,*};
+  type_migration=${migration#*,};
+  echo "Application de la migration $id_migration ($type_migration)"
+
+  if [[ "$type_migration" = "00_db_migrate" ]]
   then
-    echo ""
-    echo "Migration $file"
-    ./bin/rails db:migrate:up VERSION="${file::14}"
-  elif [[ $file == *.rake ]]
-  then
-    echo ""
-    name=${file%.*}
-    suffix=${name: -5}
-    if [[ $suffix == _spec ]]
-    then
-      echo "Task spec ${file}"
-      ./bin/rake spec/lib/tasks/deployment/${file}
-    else
-      echo "Task after_party ${name:15}"
-      ./bin/rake after_party:${name:15}
+    # Une erreur de migration est bloquante
+    if ! bin/rails db:migrate VERSION=$id_migration; then
+      echo "Erreur lors de la migration de schéma $id_migration. Analyse humaine nécessaire."
+      exit 1
     fi
+  elif [[ "$type_migration" = "01_after_party" ]]
+  then
+    bin/rake after_party:run_version VERSION=$id_migration
+    echo "Code retour after party : $?"
   fi
-done < $FILE_LIST
-echo ""
-echo "Processing data done."
+  #sinon, ignorer
+done
